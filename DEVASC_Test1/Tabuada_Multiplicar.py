b85a259b4cacabb9200f9408112290fee754694d
fortunatofelix <fortunatofelix2015@gmail.com>
2021-12-22 22:09:24 -0800

    Commit Tabuada

---
+++ DEVASC_Test1/Tabuada_Multiplicar.py
@@ -0,0 +1,6 @@
+for num1 in range(2,11):
+        print('')
+        print('------------------------')
+        print('Esta e a TABUADA dos ', num1)
+        for num2 in range(1,11):
+            print(num1*num2)
